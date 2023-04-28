11758ec9bf2291524269f5224090605835985f9d
jad610 <87204388+jad610@users.noreply.github.com>
2023-04-27 19:05:19 -0700

    Update example.js

--- tree map/example.js
+++ tree map/example.js
@@ -97,7 +97,7 @@ d3.json("top20_overall_all.json").then(function (data) {
 // create legend
 var legend = svg.append("g")
 .attr("class", "legend")
-.attr("transform", "translate(" + (width + 100) + ", 50)");
+.attr("transform", "translate(" + (width + 120) + ", 20)");
 
 // legend title
 legend.append("text")
