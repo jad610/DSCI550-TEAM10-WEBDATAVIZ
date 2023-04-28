0a3e3b6c9e98a3d9a89353bb24620a0d3e643916
jad610 <87204388+jad610@users.noreply.github.com>
2023-04-27 18:56:11 -0700

    Update wordcloud.js

--- wordcloud/wordcloud.js
+++ wordcloud/wordcloud.js
@@ -55,7 +55,7 @@ function draw(words) {
 // create legend
 var legend = svg.append("g")
   .attr("class", "legend")
-  .attr("transform", "translate(" + (width + 200) + "," + (height + 30) + ")");
+  .attr("transform", "translate(" + (width + 50) + "," + (height + 30) + ")");
 
 legend.append("text")
   .text("Legend:")
