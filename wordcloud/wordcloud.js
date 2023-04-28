5ab00cbc0017478c5ce6e051530bdbfedd72c8b7
jad610 <87204388+jad610@users.noreply.github.com>
2023-04-27 19:21:00 -0700

    Update wordcloud.js

--- wordcloud/wordcloud.js
+++ wordcloud/wordcloud.js
@@ -6,7 +6,7 @@ var margin = {top: 5, right: 5, bottom: 5, left: 5},
 // create svg object
 var svg = d3.select("#my_dataviz").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom + )
+    .attr("height", height + margin.top + margin.bottom + 200)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
